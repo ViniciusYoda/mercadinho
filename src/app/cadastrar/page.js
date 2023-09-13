1387dc93db04f124bf798535677309643459912f
Vinícius Yoda <vyoda4604@gmail.com>
2023-09-13 08:58:59 -0300

    arr

--- src/app/cadastrar/page.js
+++ src/app/cadastrar/page.js
@@ -1,4 +1,4 @@
-"use server"
+"use client"
 
 import { useRouter } from 'next/router';
 import Button from '@/components/Button';
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import Header from "@/components/header";
+import Header from "@/components/Header";
 import Login from "./login/page";
 import Principal from "./principal/page";
 // import Cart from "./cart/page"
